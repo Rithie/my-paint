d065ac4007277c6365fc96d562c0c8023944a6d3
Rithie <euler.sistemas@gmail.com>
2016-10-23 14:08:13 -0200

    project core added

---
+++ src/paintbrushapplication/FramePrincipal.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paintbrushapplication;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author Rithie7
+ */
+
+public class FramePrincipal extends JFrame{
+    
+    public FramePrincipal()
+    {
+      
+       this.setSize(400,400);
+       this.setLocationRelativeTo(null);
+       this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+       this.setTitle("paint");
+       
+   
+       this.add(new Tela());
+     
+       this.setVisible(true);
+    }
+}
+
+
+
+
+
+
+
+    
+    
+
+   
+    
+   
+

a2a92babc9416389b58f812340371666068c8c5f
Rithie <euler.sistemas@gmail.com>
2016-10-23 15:26:39 -0200

    added new buttons with icons

--- src/paintbrushapplication/Tela.java
+++ src/paintbrushapplication/Tela.java
@@ -5,8 +5,13 @@
  */
 package paintbrushapplication;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -15,12 +20,102 @@ import javax.swing.JPanel;
  */
 class Tela extends JPanel implements ActionListener {
 
+
+    public int x, action = 0;
+    public int y;
+    public int w;
+    public int h;
+
+    Color colorBK = (Color.BLACK), colorWH = (Color.WHITE);
+
+    JButton linhabtn;
+    JButton rectangbtn;
+    JButton circunferenciabtn;
+    JButton freestylebtn;
+    JButton pontobtn;
+
+    JButton cbtn, fbtn, SMbtn, Ubtn, Rbtn, Sbtn, Lbtn,SEbtn,Dbtn;
+
     public Tela() {
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        this.setLayout(new BorderLayout());
+        this.setBackground(Color.WHITE);
+
+        JPanel btnPanel = new JPanel();
+        btnPanel.setBackground(Color.LIGHT_GRAY);
+        JPanel btnPanel1 = new JPanel();
+        btnPanel1.setBackground(Color.BLUE);
+
+        Box thebox = Box.createVerticalBox();//Box.createHorizontalBox();
+        //Box thebox1 = Box.createVerticalBox();
+
+        // Criando e definindo icones dos botoes
+        pontobtn = new JButton(""); // ponto
+        pontobtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/paintbrushapplication/img/ponto_icon.png")));
+
+        freestylebtn = new JButton(""); // lapis freestylebtn
+        freestylebtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/paintbrushapplication/img/lapis_icon.png")));
+
+        linhabtn = new JButton(""); // linha
+        linhabtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/paintbrushapplication/img/reta_icon.png")));
+
+        rectangbtn = new JButton(""); // retangulo
+        rectangbtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/paintbrushapplication/img/rect_icon.png")));
+
+        circunferenciabtn = new JButton(""); // circunferencia
+        circunferenciabtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/paintbrushapplication/img/circunferencia_icon.png")));
+        //cbtn = new JButton("Cores");
+
+//        SMbtn = new JButton("Move  ");
+//        Ubtn = new JButton("Undo  ");
+//        Rbtn = new JButton("Redo  ");
+//        Sbtn = new JButton("Save  ");
+//        Lbtn = new JButton("Load  ");
+//        SEbtn = new JButton("Select");
+//        Dbtn = new JButton("Delete");
+
+        // adicionando listeners
+        pontobtn.addActionListener(this);
+        freestylebtn.addActionListener(this);
+        linhabtn.addActionListener(this);
+        rectangbtn.addActionListener(this);
+        circunferenciabtn.addActionListener(this);
+
+//        SMbtn.addActionListener(this);
+//        Ubtn.addActionListener(this);
+//        Rbtn.addActionListener(this);
+//        Sbtn.addActionListener(this);
+//        Lbtn.addActionListener(this);
+//        SEbtn.addActionListener(this);
+//        Dbtn.addActionListener(this);
+
+        // adicionando na caixa
+        thebox.add(pontobtn);
+        thebox.add(freestylebtn);
+        thebox.add(linhabtn);
+        thebox.add(rectangbtn);
+        thebox.add(circunferenciabtn);
+
+//        thebox.add(cbtn);
+//        thebox1.add(Lbtn);
+//        thebox1.add(Sbtn);
+//        thebox1.add(SEbtn);
+//        thebox1.add(Ubtn);
+//        thebox1.add(Rbtn);
+//        thebox1.add(SMbtn);
+//        thebox1.add(Dbtn);
+
+        btnPanel1.add(thebox);
+//        btnPanel1.add(thebox1);
+
+        this.add(btnPanel, BorderLayout.SOUTH);
+        this.add(btnPanel1, BorderLayout.WEST);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
